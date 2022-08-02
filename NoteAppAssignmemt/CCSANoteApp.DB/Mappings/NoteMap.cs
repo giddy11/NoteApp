910ad087f27c0b087cc5417714c6203277f9225b
gideon <edoghotugiddy@gmail.com>
2022-08-02 18:50:31 +0100

    setting up the note map and user map

---
+++ NoteAppAssignmemt/CCSANoteApp.DB/Mappings/NoteMap.cs
@@ -0,0 +1,19 @@
+﻿using CCSANoteApp.Domain;
+using FluentNHibernate.Mapping;
+
+namespace CCSANoteApp.DB.Mappings
+{
+    public class NoteMap : ClassMap<Note>
+    {
+        public NoteMap()
+        {
+            Id(note => note.Id);
+            Map(note => note.NoteCreator);
+            Map(note => note.Title);
+            Map(note => note.Content);
+            Map(note => note.Group);
+            Map(note => note.CreatedDate);
+            Map(note => note.UpdatedDate);
+        }
+    }
+}
