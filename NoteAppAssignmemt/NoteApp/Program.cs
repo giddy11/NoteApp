d0a206bc5c5820883db9afe25c5bfe5e10352415
gideon <edoghotugiddy@gmail.com>
2022-08-02 19:08:39 +0100

    added the session factory service, IUserservice and INotesewrvice to the program.cs

--- NoteAppAssignmemt/NoteApp/Program.cs
+++ NoteAppAssignmemt/NoteApp/Program.cs
@@ -1,3 +1,6 @@
+using CCSANoteApp.DB;
+using CCSANoteApp.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<SessionFactory>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<INoteService, NoteService>();
 
 var app = builder.Build();
 
